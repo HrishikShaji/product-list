6f57019ecb11bfae648a549c200ff5774c10260f
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-11 12:56:43 +0530

    beckend completed

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,22 +1,38 @@
 import { db } from "@/lib/db";
 
+const getAllProductsInCategory = async (categoryId) => {
+  const productsInCategory = await db.product.findMany({
+    where: {
+      categoryId: categoryId,
+    },
+  });
+
+  const subcategories = await db.category.findMany({
+    where: {
+      parentId: categoryId,
+    },
+  });
+
+  const subcategoryProducts = await Promise.all(
+    subcategories.map((subcategory) =>
+      getAllProductsInCategory(subcategory.id),
+    ),
+  );
+
+  return productsInCategory.concat(subcategoryProducts.flat());
+};
+
 export const GET = async (req: Request) => {
   const { searchParams } = new URL(req.url);
   const categoryId = searchParams.get("categoryId");
   try {
     if (categoryId) {
-      const products = await db.product.findMany({
-        where: {
-          categoryId: categoryId,
-        },
-      });
-
+      const allProducts = await getAllProductsInCategory(categoryId);
+      return new Response(JSON.stringify(allProducts));
+    } else {
+      const products = await db.product.findMany({});
       return new Response(JSON.stringify(products));
     }
-
-    const products = await db.product.findMany({});
-
-    return new Response(JSON.stringify(products));
   } catch (error) {
     return new Response("Error");
   }
